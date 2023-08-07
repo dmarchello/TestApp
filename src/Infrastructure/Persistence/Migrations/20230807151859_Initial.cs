df5427a05a062d5051d4a3de42c16faab2f1ee7e
Denys Marchenko <denys.marchenko@intellias.com>
2023-08-07 18:20:00 +0300

    Added DB Init migration

---
+++ src/Infrastructure/Persistence/Migrations/20230807151859_Initial.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TestApp.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coordinate",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    X = table.Column<int>(type: "int", nullable: false),
+                    Y = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coordinate", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rectangles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TopLeftId = table.Column<int>(type: "int", nullable: false),
+                    BottomRightId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rectangles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Rectangles_Coordinate_BottomRightId",
+                        column: x => x.BottomRightId,
+                        principalTable: "Coordinate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Rectangles_Coordinate_TopLeftId",
+                        column: x => x.TopLeftId,
+                        principalTable: "Coordinate",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Coordinate_X",
+                table: "Coordinate",
+                column: "X");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Coordinate_Y",
+                table: "Coordinate",
+                column: "Y");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rectangles_BottomRightId",
+                table: "Rectangles",
+                column: "BottomRightId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rectangles_TopLeftId",
+                table: "Rectangles",
+                column: "TopLeftId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Rectangles");
+
+            migrationBuilder.DropTable(
+                name: "Coordinate");
+        }
+    }
+}
